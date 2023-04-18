a895b49aa67591149fe677bbc19326dd8a3a570c
LauraZiem <laura.ziem12@gmail.com>
2023-04-18 19:47:15 -0300

    Primeiro commit

---
+++ DoToo/DoToo/Bootstrapper.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+using System.Linq;
+using System.Reflection;
+using Xamarin.Forms;
+using DoToo.Views;
+using DoToo.Repositories;
+using DoToo.ViewModels;
+
+namespace DoToo
+{
+    public abstract class Bootstrapper
+    {
+
+        protected ContainerBuilder ContainerBuilder 
+        { get; private set; }
+
+        protected Bootstrapper()
+        {
+            Initialize();
+            FinishInitialization();
+        }
+
+        protected virtual void Initialize()
+        { 
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            ContainerBuilder = new ContainerBuilder();
+
+            var assemblyList =
+                currentAssembly.DefinedTypes
+                .Where( e => e.IsSubclassOf(typeof(Page)) || 
+                             e.IsSubclassOf(typeof(ViewModel)) );
+
+            foreach ( var type in assemblyList )
+            {
+                ContainerBuilder.RegisterType( type.AsType() );
+            }
+
+            ContainerBuilder.RegisterType<TodoItemRepository>()
+                .SingleInstance();
+
+        }
+
+        private void FinishInitialization()
+        { 
+            var container = ContainerBuilder.Build();
+            Resolver.Initialize( container );
+        }
+    }
+}
